659b472123b176fc84448a07266ba710bb78b417
Chris Bartle <git@chrisbartle.com>
2020-12-08 17:00:03 -0700

    Added Stunner, implemented basic NAT identification call

--- NATConnectivityAnalyzer.cpp
+++ NATConnectivityAnalyzer.cpp
@@ -1,4 +1,5 @@
 #include "NATConnectivityAnalyzer.h"
+#include "Stunner/StunClientHelper.h"
 
 NATConnectivityAnalyzer::NATConnectivityAnalyzer(QWidget *parent)
     : QMainWindow(parent)
@@ -17,4 +18,44 @@ NATConnectivityAnalyzer::NATConnectivityAnalyzer(QWidget *parent)
                 internalIPString.append(list[rut].toString());
             }
     ui.internalIP->setText(internalIPString);
+
+    //Get the STUN info
+	QString natTypeString;
+    CStunClientHelper clientHelper("stun.xten.com");
+    NAT_TYPE natType = clientHelper.GetNatType();
+	switch (natType)
+	{
+	case NAT_TYPE::ERROR_DETECTING_NAT:
+		natTypeString = "There was an error detecting NAT.";
+		break;
+
+	case NAT_TYPE::FIREWALL_BLOCKS_UDP:
+		natTypeString = "There is a firewall that blocks UDP.";
+		break;
+
+	case NAT_TYPE::FULL_CONE_NAT:
+		natTypeString = "The NAT type is Full Cone NAT.";
+		break;
+
+	case NAT_TYPE::OPEN_INTERNET:
+		natTypeString = "There is no NAT and directly on Open Internet.";
+		break;
+
+	case NAT_TYPE::RESTRICTED_CONE_NAT:
+		natTypeString = "The NAT type is Restricted Cone NAT.";
+		break;
+
+	case NAT_TYPE::RESTRICTED_PORT_CONE_NAT:
+		natTypeString = "The NAT type is Restricted Port Cone NAT.";
+		break;
+
+	case NAT_TYPE::SYMMETRIC_NAT:
+		natTypeString = "The NAT type is Symmetric NAT.";
+		break;
+
+	case NAT_TYPE::SYMMETRIC_UDP_FIREWALL:
+		natTypeString = "There is a symmetric UDP firewall.";
+		break;
+	}
+	ui.natType->setText(natTypeString);
 }
